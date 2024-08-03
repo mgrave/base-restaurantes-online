8648a083c32b756c8122a87d741f19b034427071
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-02 19:52:28 -0500

    seed de countries

---
+++ src/seed/seed-countries.ts
@@ -0,0 +1,12 @@
+interface SeedCountry {
+    name: string;
+    id: string;
+  }
+  
+  export const countries: SeedCountry[] = [
+   
+  //  { name: "Mexico", id: "MX" },
+   
+    { name: "United States", id: "US" },
+    
+  ];
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,12 +1,15 @@
 import prisma from '../lib/prisma';
 import { initialData } from './seed';
+import { countries } from './seed-countries';
 
 async function main() {
 
     await Promise.all([
+        await prisma.user.deleteMany(),
+        await prisma.country.deleteMany(),
+       
         await prisma.productImage.deleteMany(),
         await prisma.product.deleteMany(),
-        await prisma.user.deleteMany(),
         await prisma.category.deleteMany()
 
     ]);
@@ -23,6 +26,10 @@ async function main() {
     await prisma.user.createMany({
         data: users
     });
+
+    await prisma.country.createMany({
+        data: countries
+    })
 
     const categoriesData = categories.map(category => ({
         //vamos a regresar un objeto directamente

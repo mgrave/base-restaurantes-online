bd69368d84e9baabebd88d63e0cd7746afc82eea
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2024-08-16 09:47:59 -0500

    listado de productos

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -152,7 +152,8 @@ const isAdmin = (session?.user.role === 'admin');
             <div className='w-full h-px bg-gray-200 my-10'></div>
 
                 <Link 
-                href="/"
+                href="/admin/products"
+                onClick={() => closeMenu()}
                 className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
                 >
                     <IoShirtOutline 
